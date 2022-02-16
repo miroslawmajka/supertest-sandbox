ab92ad18318648e2b324b1b26a9559fc11e699a7
miroslawmajka <go2mirek@gmail.com>
2022-02-16 20:24:23 +0000

    MIREK first commit

---
+++ server.spec.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+
+const app = require('./server');
+
+it('works', () => {
+  request(app)
+    .get('/user')
+    .expect('Content-Type', /json/)
+    .expect('Content-Length', '15')
+    .expect(200)
+    .end(function (err, res) {
+      if (err) throw err;
+    });
+});
